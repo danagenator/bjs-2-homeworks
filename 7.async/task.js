8c894deaec2257feba2eb03bfd548364816122d8
danakras <danagekras@gmail.com>
2024-12-13 17:19:08 +0400

    дз выполнено

---
+++ 7.async/task.js
@@ -0,0 +1,52 @@
+class AlarmClock {
+    constructor() {
+        this.alarmCollection = [];
+        this.intervalId = null;
+    }
+
+    addClock(time, callback) {
+        if (!time || !callback) {
+            throw new Error('Отсутствуют обязательные аргументы');
+        } else if (this.alarmCollection.find(alarm => alarm.time === time)) {
+            console.warn('Уже присутствует звонок на это же время')
+        }
+        this.alarmCollection.push({ time, callback, canCall: true })
+    }
+
+    removeClock(time) {
+        this.alarmCollection = this.alarmCollection.filter(
+            alarm => alarm.time !== time)
+    }
+
+    getCurrentFormattedTime() {
+        return new Date().toLocaleTimeString().slice(0, -3);
+    }
+
+    start() {
+        if (this.intervalId) {
+            return
+        }
+        this.intervalId = setInterval(() => {
+            this.alarmCollection.forEach(alarm => {
+                if (alarm.time === this.getCurrentFormattedTime() && alarm.canCall) {
+                    alarm.canCall = false;
+                    alarm.callback();
+                }
+            })
+        }, 1000);
+    }
+
+    stop() {
+        clearInterval;
+        this.intervalId = null;
+    }
+
+    resetAllCalls() {
+        this.alarmCollection.forEach(alarm => alarm.canCall = true);
+    }
+
+    clearAlarms() {
+        this.stop();
+        this.alarmCollection = [];
+    }
+} 
